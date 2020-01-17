354c88a39c51bed470f9df7b8ab797066e77ef75
adifek <tom.przerwa@gmail.com>
2020-01-17 01:30:53 +0100

    /permissions POST, PATCH, DELETE with basic validation

--- dbmodels.py
+++ dbmodels.py
@@ -1,10 +1,16 @@
+from enum import Enum
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 from db import Base
+
+
+class EventType(str, Enum):
+    CREATED = 'CREATED'
+    UPDATED = 'UPDATED'
+    DELETED = 'DELETED'
 
 
 class Event(Base):
@@ -13,14 +19,35 @@ class Event(Base):
     id = Column(String, primary_key=True, index=True)
     aggregation_id = Column(String, index=True)
     utctime = Column(String, index=True)
-    type = Column(String, index=True)
+    type = Column(String, index=True)  # CREATED | UPDATED | DELETED
     data = Column(String)
 
     @classmethod
-    def create(cls, aggregation_id: str, type: str, data: str):
+    def create(cls, aggregation_id: str, type: EventType, data: str) -> 'Event':
         d = datetime.utcnow()
         return cls(id=str(uuid4()), aggregation_id=aggregation_id, utctime=str(d), type=type, data=data)
 
+    @classmethod
+    def exists(cls, aggregation_id, db) -> bool:
+        return bool(db.query(cls).filter(aggregation_id == aggregation_id).first())
 
 
+class EventManager:
+    def __init__(self, db):
+        self.db = db
 
+    def can_insert(self, event: Event):
+        event_type = EventType[event.type]
+        aggregation_id = event.aggregation_id
+        if event_type is EventType.CREATED:
+            return not self._is_created(aggregation_id)
+        else:
+            return self._is_created(aggregation_id) and not self._is_deleted(aggregation_id)
+
+    def _is_created(self, aggregation_id: str) -> bool:
+        return bool(self.db.query(Event).filter(Event.aggregation_id == aggregation_id).first())
+
+    def _is_deleted(self, aggregation_id: str) -> bool:
+        return bool(self.db.query(Event).filter(
+            (Event.aggregation_id == aggregation_id) & (Event.type == EventType.DELETED.value)
+        ).first())
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from enum import Enum
 from json import dumps
+from uuid import UUID
 
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 
 from sqlalchemy.orm import Session
 
 from db import Base, SessionLocal, engine
-from dbmodels import Event
+from dbmodels import Event, EventType, EventManager
 
 
 Base.metadata.create_all(bind=engine)
@@ -25,21 +27,20 @@ def get_db():
 
 
 @app.get('/events')
-async def get_events():
-    """GET list of all events"""
-    return []
+async def get_events(aggregation_id: str = None, db: Session = Depends(get_db)):
+    """GET list of events"""
+    if aggregation_id is not None:
+        events = db.query(Event).filter(Event.aggregation_id == aggregation_id).all()
+    else:
+        events = db.query(Event).all()
+    return events or HTTP_404_NOT_FOUND
 
 
-@app.get('/events/{_id}')
-async def get_event(id: int):
+@app.get('/events/{id}')
+async def get_event(id: UUID, db: Session = Depends(get_db)):
     """GET event by its id"""
-    return {'id': id}
-
-
-class EventType(str, Enum):
-    CREATED = 'CREATED'
-    UPDATED = 'UPDATED'
-    DELETED = 'DELETED'
+    event = db.query(Event).filter(Event.id == str(id)).first()
+    return event or HTTP_404_NOT_FOUND
 
 
 class InPermission(BaseModel):
@@ -48,14 +49,36 @@ class InPermission(BaseModel):
     value: bool
 
 
-@app.post('/permissions')
-async def create_permission(permission: InPermission,
-                            db: Session = Depends(get_db)) -> str:
-
+async def create_event(permission: InPermission, event_type: EventType, db: Session):
     event = Event.create(
         aggregation_id=permission.name,
-        type=EventType.CREATED,
+        type=event_type,
         data=dumps({'resource_type': permission.resource_type, 'value': permission.value}))
+
+    if not EventManager(db).can_insert(event):
+        return HTTP_409_CONFLICT
+
     db.add(event)
     db.commit()
     return event.id
+
+
+@app.post('/permissions', status_code=HTTP_201_CREATED)
+async def create_permission(permission: InPermission,
+                            db: Session = Depends(get_db)) -> str:
+
+    return await create_event(permission, EventType.CREATED, db)
+
+
+@app.patch('/permissions', status_code=HTTP_201_CREATED)
+async def update_permission(permission: InPermission,
+                            db: Session = Depends(get_db)) -> str:
+
+    return await create_event(permission, EventType.UPDATED, db)
+
+
+@app.delete('/permissions', status_code=HTTP_201_CREATED)
+async def delete_permission(permission: InPermission,
+                            db: Session = Depends(get_db)) -> str:
+
+    return await create_event(permission, EventType.DELETED, db)
